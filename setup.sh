cdd8fc1a52686a48058dc5d432aa01f8b3594250
Yannick Menager <ymenager@kloudtek.com>
2018-11-27 09:19:09 -0800

    Fixing build

--- setup.sh
+++ setup.sh
@@ -3,10 +3,8 @@
 chmod +x /usr/bin/jenkins-slave
 
 export DEBIAN_FRONTEND=noninteractive
-curl https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-sudo apt-add-repository "deb https://deb.nodesource.com/node_7.x $(lsb_release -sc) main"
 apt-get update
-apt-get install -yq openjdk-8-jdk curl nodejs npm git maven
+apt-get install -yq curl openjdk-8-jdk curl nodejs npm git
 echo "Creating group ${group} with GUID ${gid}"
 addgroup --gid ${gid} ${group}
 adduser --home $HOME --uid ${uid} --ingroup ${group} --disabled-password --gecos "" ${user}
