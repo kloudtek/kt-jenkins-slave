236acd5655fb4810ea447d4d3225f805bc6f8244
Yannick Menager <ymenager@kloudtek.com>
2018-11-30 19:18:03 -0800

    Set TZ to PST

--- setup.sh
+++ setup.sh
@@ -7,6 +7,13 @@ chmod +x /usr/bin/jenkins-slave
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 
+# Update timezone
+
+apt-get install -yq tzdata && true
+
+ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+dpkg-reconfigure -f noninteractive tzdata
+
 # Update locales
 
 apt-get install -yq locales && true
