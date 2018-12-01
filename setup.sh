a802ee01a0f05e8d00b016041bbf30307b461eb1
Yannick Menager <ymenager@kloudtek.com>
2018-11-30 19:12:20 -0800

    Set locale to UTF8

--- setup.sh
+++ setup.sh
@@ -6,7 +6,23 @@ chmod +x /usr/bin/jenkins-slave
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
+
+# Update locales
+
+apt-get install -yq locales && true
+locale-gen --no-purge en_US.UTF-8
+update-locale LANG=en_US.UTF-8
+echo locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8 | debconf-set-selections
+echo locales locales/default_environment_locale select en_US.UTF-8 | debconf-set-selections
+dpkg-reconfigure locales
+update-locale LANG=en_US.UTF-8
+
+# Install other packages
+
 apt-get install -yq curl openjdk-8-jdk curl nodejs npm git gpg
+
+# Configure user
+
 echo "Creating group ${group} with GUID ${gid}"
 addgroup --gid ${gid} ${group}
 adduser --home $HOME --uid ${uid} --ingroup ${group} --disabled-password --gecos "" ${user}
